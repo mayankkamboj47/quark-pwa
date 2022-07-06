2cdb49d8f8e63bfd24d41efe88480f1184a4152c
Mayank Kamboj <mayankkamboj47@gmail.com>
2022-07-06 14:25:37 +0530

    first working version

--- main.js
+++ main.js
@@ -10,5 +10,27 @@ else{
 document.querySelector('#search-form').addEventListener('submit', e => {
     e.preventDefault();
     const searchBarText = document.querySelector('#search').value;
-    fetch('./'+searchBarText);
+    fetch('./search/'+searchBarText).then(r=>r.json()).then(links => writeSearchResultLinks(links));
 });
+
+function writeSearchResultLinks(links){
+    const container = elt('section', {id : "search_results"});
+    for(let link of links)
+      container.appendChild(elt('a', {href : link}, link));
+    const existing = document.getElementById("search_results");
+    existing.parentElement.replaceChild(container, existing);
+}
+
+// HELPER FUNCTIONS
+
+function elt(name, attr, ...children){
+    const el = document.createElement(name);
+    for(let a in attr){
+        el.setAttribute(a, attr[a]);
+    }
+    for(let child of children){
+        if(typeof child==='string') el.appendChild(document.createTextNode(child));
+        else el.appendChild(child);
+    }
+    return el;
+}
--- serviceWorker.js
+++ serviceWorker.js
@@ -6,7 +6,7 @@ const offlineAssets = ['./', '/styles/index.css', './main.js'];
 /* offline Docs are files the search engine will search from. 
 Testing only. Soon, entire docs directory will be dynamically loaded. 
 */
-const offlineDocs = ['./docs/1.md', './docs/2.md', './docs/3.md'];
+const offlineDocs = ['./docs/1.html', './docs/2.html', './docs/3.html'];
 self.addEventListener('install', (e)=>{
     console.log('Service worker installed');
     e.waitUntil(addAllToCache(offlineAssets));
@@ -37,7 +37,7 @@ function addToTaggedCache(resources) {
     return caches.open('tagged').then(taggedCache => {
         return Promise.all(resources.map(resource=>{
             return fetch(resource).then((response)=>{
-                const tags = tagify(response);  // tagify is yet to be implemented.
+                // tagify is yet to be implemented.
                 // do something with tags, like store them properly in indexedDB
                 // before that, the project will probably be moved to workbox. 
                 // or is it a good decision ? Time to git it. 
@@ -46,17 +46,6 @@ function addToTaggedCache(resources) {
         }));
     });
 }
-async function cacheFirst(request){
-    const cacheRes = await caches.match(request);
-    if(cacheRes) return cacheRes;
-    const networkRes = await fetch(request);
-    const networkResClone = networkRes.clone();
-    caches.open('responses').then(responses=>{
-        responses.put(request, networkResClone);
-    });
-    return networkRes;
-}
-
 
 /**
  * To fix : We need to somehow compare the two responses in cacheFinish.then, 
@@ -65,27 +54,60 @@ async function cacheFirst(request){
  */
 function cacheNetworkRace(request){
     return new Promise((resolve, reject)=>{
-        const cacheFinish = caches.match(request).then(r=>{
-            if(r) {
+        const cacheFinish = router(request).then(r=>{
+            // we finished the response battle
+            if(r.status >=200 && r.status < 300) {
+                // but only resolve if we also won the 200-status war
                 resolve(r);
                 return r;
             }
-        }).catch(reject);
+        }).catch(()=>{});
         fetch(request).then(res=>{
             caches.open('responses').then(responses=>{
+                // once network finishes, wait for cache to finish
+                // and then cache this response if needed
                 cacheFinish.then(r=>r ? null : responses.put(request, res.clone()));
+                // but don't wait. Resolve immediately with what we have. 
                 resolve(res);
             });
         }).catch(reject);
     });
 }
 
-function tagify(string){
-    const tags = [];
-    const ignore = {' ': true, '-' : true};
-    for(let i=0;i<string.length;i++){
-        if(string[i] in ignore) continue;
-        else if(string[i]){
-        }
+async function generateResponseFromCache(query){
+    const taggedCache = await caches.open('tagged');
+    const keys = await taggedCache.keys();
+    const responses = await Promise.all(keys.map(key=>taggedCache.match(key)));
+    const responseTexts = await Promise.all(responses.map(r=>r.text()));
+    console.log(responseTexts, 'in generate response from cache');
+    const sortedByDist = responseTexts.map((text, i)=>({url :keys[i].url, dist : document_distance(text, query)})).sort((a,b)=>b.dist - a.dist).map(
+        r=>r.url
+    );
+    return new Response(JSON.stringify(sortedByDist), {'Content-Type' : 'text/json'});
+}
+
+function document_distance(d1, d2){
+    const vector = doc => doc.split(' ').reduce((obj, word)=>{
+        if(word in obj) obj[word]++;
+        else obj[word] = 1;
+        return obj;
+    }, {});
+    const dot_prod = (v1, v2)=> Object.keys(v1).reduce((sum, key)=> sum+v1[key]*(v2[key] || 0), 0);
+    const size = (x) => Object.values(vector(x)).reduce((sum, v)=>sum + v**2, 0)**0.5;
+    return dot_prod(vector(d1), vector(d2))/(size(d1)*size(d2));
+}
+
+async function router(request){
+    const routes = {
+        'https?:\/\/.+\/search\/(.+)' : generateResponseFromCache,
+    };
+    const defaultRoute = async function(request){
+        return caches.match(request).then(r=>r || new Response('404 not found :/', {status : 404, type : 'text/html'}));
+    };
+    for(let key in routes){
+        let rex = new RegExp(key).exec(request.url);
+        if(rex) console.log('match', rex);
+        if(rex) return routes[key].apply(self, rex.slice(1));
     }
+    return defaultRoute(request);
 }
